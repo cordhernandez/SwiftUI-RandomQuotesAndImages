eeed0501694225929798ffc9bba2da9632b7c853
Cordero Hernandez <cord.a.hernandez@gmail.com>
2022-03-30 16:58:41 -0700

    Adds initial Xcode project - first commit

---
+++ RandomQuoteAndImages/RandomQuoteAndImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomQuoteAndImages
+//
+//  Created by Cordero Hernandez on 3/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomQuoteAndImages/RandomQuoteAndImages/RandomQuoteAndImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomQuoteAndImagesApp.swift
+//  RandomQuoteAndImages
+//
+//  Created by Cordero Hernandez on 3/30/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomQuoteAndImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
